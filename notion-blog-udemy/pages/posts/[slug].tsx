4ac72d9070bdd7cd1d25409781c14891386e1185
sgmikami03 <sgmikami03@gmail.com>
2023-08-30 14:57:44 +0900

    [20]記事詳細を作る静的

---
+++ notion-blog-udemy/pages/posts/[slug].tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const Post = () => {
+  return (
+    <section className='container lg:px-2 px-5 lg:w-25 mx-auto mt-20'>
+      <h2 className='w-full text-2xl font-medium'>3回目の投稿です</h2>
+      <div className='border-b-2 w-1/3 border-sky-900'></div>
+      <span className='text-gray-500'>2001-00-00</span>
+      <br />
+      <p><span className="text-white bg-gray-500 rounded-xl px-2 pb-1 font-medium inline-block">Next.js</span></p>
+
+      <div className='mt-10 font-medium'>
+
+      </div>
+    </section>
+  )
+}
+
+export default Post;
