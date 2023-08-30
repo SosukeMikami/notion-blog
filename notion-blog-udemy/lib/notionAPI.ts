2b77081d5cd35f11a828a56be24084be86837ed3
sgmikami03 <sgmikami03@gmail.com>
2023-08-30 13:00:34 +0900

    [add]15 タグデータ取得

--- notion-blog-udemy/lib/notionAPI.ts
+++ notion-blog-udemy/lib/notionAPI.ts
@@ -12,17 +12,29 @@ export const getAllPosts = async () => {
 
     const allPosts = posts.results;
 
+    console.log(allPosts);
+    
     return allPosts.map((post) => {
         return getPageMetadata(post);
     });
+
+    return allPosts;
 };
 
 const getPageMetadata = (post: any) => {
+    const getTags = (tags: any) => {
+        const allTags = tags.map((tag: any) => {
+            return tag.name;
+        })
+        return allTags
+    };
+
     return {
         id: post.id,
         title: post.properties.Name.title[0].plain_text,
         Description: post.properties.Description.rich_text[0].plain_text,
         date: post.properties.Date.date.start,
         Sulg: post.properties.Sulg.rich_text[0].plain_text,
-    }
+        Tags: getTags(post.properties.Tags.multi_select)
+    };
 };
