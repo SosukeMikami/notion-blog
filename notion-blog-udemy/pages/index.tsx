deee35fed460b5e1b306aefde2c6d2422a0f77a6
sgmikami03 <sgmikami03@gmail.com>
2023-08-30 11:45:13 +0900

    [add]13 記事の情報を取得する

--- notion-blog-udemy/pages/index.tsx
+++ notion-blog-udemy/pages/index.tsx
@@ -1,9 +1,23 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { getAllPosts } from '@/lib/notionAPI'
+
+import { postType } from '@/pages/types';
+
+export const getStaticProps = async () => {
+  const allPosts: postType[] = await getAllPosts();
+  return {
+    props: {
+      allPosts,
+    },
+    revalidate: 60,
+  }
+}
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home({ allPosts }: {allPosts: postType[]}) {
+  console.log(allPosts);
+  
   return (
     <div><h1 className='text-red-900'>hogehoge</h1></div>
   )
